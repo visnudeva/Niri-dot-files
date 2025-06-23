9c09d7340c2c6c950b4b9a1ada2e898e96350b93
Visnu <109976023+visnudeva@users.noreply.github.com>
2025-06-23 22:56:48 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ NIRI_DEST="$HOME/niri"
 FUZZEL_DEST="$HOME/fuzzel"
 WALLPAPER_SOURCE="$NIRI_DEST/dotdark.png"
 WALLPAPER_DEST="/usr/share/endeavouros/backgrounds"
-PACKAGES=(yay niri kitty waybar dunst fuzzel swaybg hyprlock hypridle thunar thunar-volman gvfs geany blueman nwg-look)
+PACKAGES=(yay niri kitty waybar makko swaybg hyprlock hypridle thunar thunar-volman gvfs geany blueman nwg-look)
 AUR_PACKAGES=(ttf-nerd-fonts-symbols tofi)
 
 # --- Install necessary packages ---
