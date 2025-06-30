12376bcb70cb8f8d390e163880f0a4a09c30de8d
Visnu <109976023+visnudeva@users.noreply.github.com>
2025-06-30 23:34:52 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ NIRI_DEST="$HOME/niri"
 FUZZEL_DEST="$HOME/fuzzel"
 WALLPAPER_SOURCE="$NIRI_DEST/dotdark.png"
 WALLPAPER_DEST="/usr/share/endeavouros/backgrounds"
-PACKAGES=(yay niri waybar fuzzel swaybg swayidle gvfs geany blueman nwg-look mako alacritty
+PACKAGES=(yay niri waybar fuzzel swaybg swayidle gvfs geany blueman nwg-look mako kitty
 xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring nautilus)
 AUR_PACKAGES=(ttf-nerd-fonts-symbols swaylock-effects qogir-cursor-theme qogir-icon-theme qogir-gtk-theme)
 
